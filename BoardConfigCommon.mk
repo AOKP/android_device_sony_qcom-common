fc42f007fe30a67ccec52f009f73817a89b24d3a
Ethan Chen <intervigil@gmail.com>
2014-03-28 13:00:06 -0700

    qcom-common: Select CM power HAL
    Change-Id: I3c6a974b39073371019ee1471cf11efd2a1240ff
    
    Conflicts:
    	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,8 +23,8 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
 
-# Use the CM PowerHAL
-TARGET_USES_CM_POWERHAL := true
+# Power HAL
+TARGET_POWERHAL_VARIANT := cm
 CM_POWERHAL_EXTENSION := qcom
 
 # Lights HAL
